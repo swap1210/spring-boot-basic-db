428e8e9c895516f5212981929584e007739385fc
swap1210 <p.swap1210@gmail.com>
2025-03-25 05:22:31 -0400

    Simplified using lombok

--- src/main/java/dev/bedesi/sms/schoolmanagementsystem/entity/Course.java
+++ src/main/java/dev/bedesi/sms/schoolmanagementsystem/entity/Course.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,37 +21,4 @@ public class Course {
 
     @ManyToOne
     private Teacher teacher;
-
-    // Constructors
-    public Course() {}
-
-    public Course(String name, Teacher teacher) {
-        this.name = name;
-        this.teacher = teacher;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
 }
--- src/main/java/dev/bedesi/sms/schoolmanagementsystem/entity/Teacher.java
+++ src/main/java/dev/bedesi/sms/schoolmanagementsystem/entity/Teacher.java
@@ -4,35 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    // Constructors
-    public Teacher() {}
-
-    public Teacher(String name) {
-        this.name = name;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
